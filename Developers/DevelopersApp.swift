d7e4470677aa5b3c8f1ae1552c9fd7385364e8b5
Aleksandr Karpov <papadoster13@gmail.com>
2023-03-11 16:50:44 +0100

    Initial Commit

---
+++ Developers/DevelopersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevelopersApp.swift
+//  Developers
+//
+//  Created by Александр Карпов on 11.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct DevelopersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
